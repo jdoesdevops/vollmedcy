038dbd13069a63ecf4b7936f350df62059173277
Joao Albuquerque <udajpg@gmail.com>
2024-04-14 20:26:25 -0300

    Teste

---
+++ server/cypress.config.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    // supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    apiBaseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost:3000',
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: '../cypress/report/mochawesome-report',
+      toConsole: true,
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss"
+    },
+    projectId: "x3bmp5"
+
+  },
+});
