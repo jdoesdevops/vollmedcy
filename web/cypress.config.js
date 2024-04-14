60a74d2518d9f45bade758e5467bda43eee0b27b
Joao Albuquerque <udajpg@gmail.com>
2024-04-14 18:27:34 -0300

    test

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -14,7 +14,7 @@ module.exports = defineConfig({
     reporter: 'mochawesome',
     reporterOptions: {
       reportDir: 'cypress/results',
-      overwrite: false,
+      overwrite: true,
       html: true,
       json: false,
       timestamp: "mmddyyyy_HHMMss"
