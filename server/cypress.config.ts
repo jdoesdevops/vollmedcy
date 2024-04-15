dbfb2eef2d21fd2c42d65c1e191d8c96a14968cf
Joao Albuquerque <udajpg@gmail.com>
2024-04-15 14:33:21 -0300

    test

---
+++ server/cypress.config.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    // supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:8080',
+    defaultCommandTimeout: 25000,
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: '../cypress/report/mochawesome-report',
+      toConsole: true,
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss"
+    },
+    env: {
+      "email": "clinica@gmail.com",
+      "senha": "4321",
+      "api_login": "http://localhost:8080/auth/login",
+      "api_clinica": "http://localhost:8080/clinica",
+      "api_especialista": "http://localhost:8080/especialista",
+      "requestMode": true
+    },
+    projectId: "x3bmp5"
+
+  },
+});
