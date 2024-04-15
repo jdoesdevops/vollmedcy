3d9f9985e289dcb8693d6e26c31f532604652e7f
Joao Albuquerque <udajpg@gmail.com>
2024-04-15 17:13:34 -0300

    Test

--- cypress/e2e/server/login-api.cy.js
+++ cypress/e2e/server/login-api.cy.js
@@ -1,33 +1,112 @@
 describe('testes em API', () => {
     context('Testes em rotas com usuário autorizado', () => {
-        it('GET via url front para teste em resposta da home', () => {
-            cy.loginApi('clinica@gmail.com', '4321')
-            cy.request('GET', '/').should((response) => {
-                // expect(response.status).to.eq(200)
+        // beforeEach(() => {
+        //     cy.loginApi(Cypress.env('email'), Cypress.env('senha'))
+
+        // })
+        it('get Servidor',() =>{
+            cy.request('http://localhost:8080/clinica').then((response) =>{
+                expect(response.status).to.eq(200)
             })
+
         })
 
-        it('Deve verificar se o token de autenticação é retornado após login via POST na API', () => {
-            cy.get('@token').should('exist');
-        })
+
+        it('Cadastra clinica',()=>{
+            const reqBody = 
+                {
+                    "planoDeSaudeAceitos":[1,2,3,4],
+                    "email":"clinica@gmail.com",
+                    "nome":"Clinica de Todos",
+                    "senha":"4321",
+                    "endereco": {
+                        "cep": 76541321,
+                        "rua": "Rua 45",
+                        "numero": 2,
+                        "complemento": "casa",
+                            "estado":"Amapá"
+                    }
+                }
+              cy.request({
+                  method: 'POST',
+                  url: 'http://localhost:8080/clinica',
+                  body: reqBody,
+                  'auth': {
+                    'email': 'clinica@gmail.com',
+                    'senha': '4321'
+                  },
+            // failOnStatusCode: false
+          
+              }).then(response =>{
+                  expect(response.status).to.eq(200)
+                  expect(response.body.id).to.exist;            
+              })
+          
+          })
+
+        it('loginApi',()=>{
+            const reqBody = {
+                "email": "clinica@gmail.com",
+                "senha": "4321"
+            }
+              cy.request({
+                  method: 'POST',
+                  body: reqBody,
+                  url: 'http://localhost:8080/auth/login',
+                  'auth': {
+                    'email': 'clinica@gmail.com',
+                    'senha': '4321'
+                  },
+                  timeout: 600000,
+            // failOnStatusCode: false
+          
+              }).then(response =>{
+                  expect(response.status).to.eq(200);
+                  expect(response.body.auth).to.be.true;
+                  expect(response.body.rota).to.eq('/clinica');
+                  expect(response.body.token).to.exist;
+                //   cy.wrap(response.body.token).as('token');
+            
+              })
+          
+          })
+
+
+        // it('GET via url front para teste em resposta da home', () => {
+        //     cy.request('GET', '/', { email: "clinica@gmail.com", senha: "4321" 
+
+        //     }).should((response) => {
+        //         expect(response.status).to.eq(200)
+        //     })
+        // })
+
+        // it('Deve verificar se o token de autenticação é retornado após login via POST na API', () => {
+        //     cy.get('@token').should('exist');
+        // })
     });
 
-    context('Validações em respostas da API', ()=>{
-      it('Requisição incorreta em criação de especialista', ()=>{
-            cy.request({
-                method: 'POST',
-                url: 'http://localhost:8080/clinica',
-                body: {
-                    nome: 'Camila',
-                    email: 'camila123@exemplo',
-                },
-            }).then((response)=>{
-                // expect(response.status).to.eq(500)
-                expect(response.body).to.have.property('message')
-            })
+    // context('Validações em respostas da API', ()=>{
+    //     // beforeEach(() =>{
+    //     //     cy.loginApi(Cypress.env('email'), Cypress.env('senha'))
+    //     // })
 
-        })
+    //     it('Requisição incorreta em criação de especialista', ()=>{
+    //         cy.request({
+    //             method: 'POST',
+    //             url: Cypress.env('api_clinica'),
+    //             body: {
+    //                 nome: 'Camila',
+    //                 email: 'camila123@exemplo',
+    //             },
+    //         failOnStatusCode: false
+            
+    //         }).then((response)=>{
+    //             expect(response.status).to.eq(500)
+    //             expect(response.body).to.have.property('message')
+    //         })
 
-    })
+    //     })
+
+    // })
 
 })
